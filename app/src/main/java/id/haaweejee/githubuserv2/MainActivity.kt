e3858b1e456df67d80e3f9022201f7a9198b42b6
magangkompas <52823787+haaweejee@users.noreply.github.com>
2022-01-17 23:01:30 +0700

    add splash screen

--- app/src/main/java/id/haaweejee/githubuserv2/MainActivity.kt
+++ app/src/main/java/id/haaweejee/githubuserv2/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_GithubUserV2)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
